2613a27ba498ea4e9e26dd74cc4e50c0cdeda3b8
Veldanov <anton.veldanov@gmail.com>
2019-01-31 12:06:19 -0800

    first commit

---
+++ plinko/score.js
@@ -0,0 +1,8 @@
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+  // Runs every time a balls drops into a bucket
+}
+
+function runAnalysis() {
+  // Write code here to analyze stuff
+}
+
