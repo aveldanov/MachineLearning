5cafdb5343330914763993dd2fe0c697dc2d88e1
Veldanov <anton.veldanov@gmail.com>
2019-02-03 18:35:43 -0800

    ML

--- plinko/score.js
+++ plinko/score.js
@@ -1,6 +1,6 @@
 const outputs = [];
 
-const predictionPoint = 300;
+//const predictionPoint = 300;
 const k = 3;
 
 
@@ -16,9 +16,26 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 
 function runAnalysis() {
   // Write code here to analyze stuff
+  const testSetSize = 10;
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+  //console.log(testSet, trainingSet);
+  let numberCorrect = 0;
+  for (let i = 0; i < testSet.length; i++) {
+    const bucket = knn(trainingSet, testSet[i][0]);
+    if (bucket === testSet[i][3]) {
+      numberCorrect++;
+    }
+    //console.log(bucket, testSet[i][3]);
 
-  const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
+  }
+  console.log("Accuracy: ", numberCorrect / testSetSize);
+
+
+}
+//source - Training Data - trying to predict
+function knn(data, point) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -27,15 +44,23 @@ function runAnalysis() {
     .last()
     .first()
     .parseInt()
-    .value()
-
-  console.log("Your point will probably fall into ", bucket);
-
+    .value();
 }
 
 
+//pointB - prediction point. pointA - actual point of a throw
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+}
+
+//testCount - number of points to test
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+  const testSet = _.slice(shuffled, 0, testCount);
+  //from testCount to a very end of the array
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
 
 }
